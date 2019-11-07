d60222463e6aa4291a3ad78b514a8248bcd1ce01
Dan King <dan.king@vptech.io>
2019-11-06 21:34:22 -0800

    Added logic and workflow for uploading a JSON file and making a copy

--- routes/load-contest.js
+++ routes/load-contest.js
@@ -12,9 +12,36 @@ router.get('/', function (req, res) {
 
 // Preview Contest
 router.post('/preview', function (req, res) {
-    var message = req.body.message
-    res.render('preview-contest', {
-        message: message
+    var fs = require('fs')
+    var formidable = require('formidable')
+    var form = new formidable.IncomingForm()
+    form.encoding = 'utf-8';
+    form.uploadDir = "./data/uploads/raw";
+    form.parse(req, function (err, fields, files) {
+        //console.log('form fields', fields)
+        //console.log('files', files)
+
+        // If not a JSON file go back via message
+        if (files.file_to_upload.type !== 'application/json') {
+            var message = 'Please upload a JSON file (.json)'
+            res.render('load-contest', {
+                message: message
+            })
+        } else {
+            // TODO halt if not files.file_to_upload.type: 'application/json',
+            var oldpath = files.file_to_upload.path
+            var newpath = './data/uploads/' + files.file_to_upload.name
+            // console.log('oldpath', oldpath)
+            // console.log('newpath', newpath)
+            fs.copyFile(oldpath, newpath, function (err) {
+                if (err) throw err
+                console.log('File uploaded and copy!')
+                var message = req.body.message
+                res.render('preview-contest', {
+                    message: message
+                })
+            })
+        }
     })
 })
 
