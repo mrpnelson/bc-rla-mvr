12b03bfc32e82f03ebb4baf35eb25d395939eaef
Dan King <dan.king@vptech.io>
2019-11-12 15:31:26 -0800

    Update contest export page with logic to handle new JSON output format

--- routes/export-contest.js
+++ routes/export-contest.js
@@ -9,9 +9,9 @@ router.get('/', function (req, res) {
     var filepath = './data/contest/contests.json'
     var contest = fs.readFileSync(filepath, 'utf8')
     var contest_json = JSON.parse(contest)
-    var contest_desc = contest_json.Description
-    var contest_id = contest_json.Id
-    let contest_details = contest_desc + ' (id: ' + contest_id + ")"
+    var contest_desc = contest_json.description
+    var contest_id = contest_json.id
+    var contest_details = contest_desc + ' (id: ' + contest_id + ")"
 
     // Read candidate list
     var filepath = './data/contest/candidates.json'
@@ -29,8 +29,8 @@ router.get('/', function (req, res) {
     // Create 'contests' object and populate
     var contests = []
     var contest_obj = {}
-    contest_obj.description = contest_json.Description
-    contest_obj.id = contest_json.Id
+    contest_obj.description = contest_json.description
+    contest_obj.id = contest_json.id
     contests.push(contest_obj)
     export_data.contests = contests
 
@@ -39,8 +39,8 @@ router.get('/', function (req, res) {
     var candidate_list_obj = JSON.parse(candidate_list)
     for (let candidate of candidate_list_obj) {
         var candidates_obj = {}
-        candidates_obj.description = candidate.Description
-        candidates_obj.id = candidate.Id
+        candidates_obj.description = candidate.description
+        candidates_obj.id = candidate.id
         candidates.push(candidates_obj)
     }
     export_data.candidates = candidates
@@ -74,7 +74,7 @@ router.get('/', function (req, res) {
             }
         },
                 */
-                console.log('ballot_contents_json', ballot_contents_json)
+                //console.log('ballot_contents_json', ballot_contents_json)
                 //ballots.push(ballot)
                 ballots.push(ballot_contents_json)
             }
@@ -82,9 +82,9 @@ router.get('/', function (req, res) {
         export_data.ballots = ballots
 
         // Write results to disk.
-        var export_filename = 'mvr.json'
-        console.log("export_data",export_data)
-        console.log("export_data_string",JSON.stringify(export_data))
+        var export_filename = 'mvr_output.json'
+        //console.log("export_data",export_data)
+        //console.log("export_data_string",JSON.stringify(export_data))
         var export_data_string = JSON.stringify(export_data)
         fs.writeFile ("public/output/"+export_filename, export_data_string, function(err) {
             if (err) throw err
